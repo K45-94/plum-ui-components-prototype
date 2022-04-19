beac622e3c5fd362c6f8817ec9b11a911052b6c4
K45-94 <kelvinhiuhu@live.com>
2022-04-19 05:06:10 +0300

    message

---
+++ src/boot/router.js
@@ -0,0 +1,15 @@
+import { boot } from "quasar/wrappers";
+import store from "src/plumStore";
+
+export default boot(({ router }) => {
+  router.afterEach((to, from) => {
+    let fromRootPath = from.path.split("/")[1],
+      toRootPath = to.path.split("/")[1];
+
+    if (fromRootPath !== toRootPath) {
+      store.state.usePageTransition = false;
+    } else {
+      store.state.usePageTransition = true;
+    }
+  });
+});

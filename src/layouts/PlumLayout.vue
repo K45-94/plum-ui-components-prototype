ae5b18652e2dcef13bee251bbc63559854493c99
K45-94 <kelvinhiuhu@live.com>
2022-04-19 11:36:17 +0300

    message

--- src/layouts/PlumLayout.vue
+++ src/layouts/PlumLayout.vue
@@ -3,11 +3,12 @@
     <q-page-container class="bg-white">
       <q-footer class="small-screen-only" bordered>
         <q-tabs class="text-white" active-color="secondary">
-          <q-route-tab to="/feeds" icon="eva-home" />
-          <q-route-tab to="/search" icon="eva-search" />
-          <q-route-tab to="/camera" icon="eva-camera" />
-          <q-route-tab to="/light" icon="eva-bulb" />
-          <q-route-tab to="/profile" icon="eva-square" />
+          <q-route-tab
+            v-for="navItem in store.state.navItems"
+            :key="navItem.root"
+            :to="navItem.to"
+            :icon="navItem.icon"
+          />
         </q-tabs>
       </q-footer>
       <router-view v-slot="{ Component }">
--- src/plumStore/index.js
+++ src/plumStore/index.js
@@ -1,6 +1,33 @@
 import { reactive } from "vue";
 
 const state = reactive({
+  navItems: [
+    {
+      root: "/feeds",
+      to: "/feeds",
+      icon: "eva-home",
+    },
+    {
+      root: "/search",
+      to: "/search",
+      icon: "eva-search",
+    },
+    {
+      root: "/camera",
+      to: "/camera",
+      icon: "eva-camera",
+    },
+    {
+      root: "/light",
+      to: "/light",
+      icon: "eva-bulb",
+    },
+    {
+      root: "/profile",
+      to: "/profile",
+      icon: "eva-square",
+    },
+  ],
   usePageTransition: true,
 });
 

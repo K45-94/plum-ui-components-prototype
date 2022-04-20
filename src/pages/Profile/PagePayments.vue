b8cc99abcbfb9d2ad133f3113d55399db0de0ebb
K45-94 <kelvinhiuhu@live.com>
2022-04-20 14:37:18 +0300

    message

--- src/pages/Profile/PagePayments.vue
+++ src/pages/Profile/PagePayments.vue
@@ -8,11 +8,129 @@
     </page-header>
     <page-body>
       <div class="q-pt-lg q-pb-md q-pl-lg q-pr-lg">
-        <p v-for="i in 30" :key="i">
-          Building Plum with vue 3. This app is getting complex and I thought I
-          could redesign and rebuild it better with Vue 3 Finally found a way
-          for nesting child pages in routes.
-        </p>
+        <div class="col-12 col-sm-8">
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> M </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >Mpesa</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/mpesawallet"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                >
+                  <q-badge color="green" floating>5</q-badge>
+                </q-btn>
+              </q-item-section>
+            </q-item>
+          </q-card>
+          <q-space class="q-pa-sm" />
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> A </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >Airtel Money</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/AirtelWallet"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                />
+              </q-item-section>
+            </q-item>
+          </q-card>
+          <q-space class="q-pa-sm" />
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> T </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >T-kash</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/TelkomWallet"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                />
+              </q-item-section>
+            </q-item>
+          </q-card>
+          <q-space class="q-pa-sm" />
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> PP </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >PayPal</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/PayPalWallet"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                >
+                  <q-badge color="blue" floating>3</q-badge>
+                </q-btn>
+              </q-item-section>
+            </q-item>
+          </q-card>
+
+          <q-space class="q-pa-sm" />
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> P </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >Pesalink</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/Pesalink"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                >
+                  <q-badge color="orange" floating>2</q-badge>
+                </q-btn>
+              </q-item-section>
+            </q-item>
+          </q-card>
+        </div>
       </div>
     </page-body>
   </page>
@@ -29,3 +147,8 @@ export default defineComponent({
   name: "PagePayments",
 });
 </script>
+
+<style lang="sass">
+.q-card
+  background: linear-gradient(90deg,#CCC5B9 79%,#CCC5B9 15%)
+</style>

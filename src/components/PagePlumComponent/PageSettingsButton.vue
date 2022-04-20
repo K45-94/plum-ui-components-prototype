d9ac791626d545b60e9ccf8ee9fd14f72a125569
K45-94 <kelvinhiuhu@live.com>
2022-04-20 16:40:04 +0300

    message

---
+++ src/components/PagePlumComponent/PageSettingsButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-btn
+    class="absolute-right"
+    unelevated
+    rounded
+    icon="eva-settings-2"
+    flat
+    dense
+    color="primary"
+  />
+</template>
+
+<script>
+export default {
+  name: "PageSettingsButton",
+};
+</script>
+
+<style lang="sass"></style>

56736fd661d054cd0f693cad0b5b2f96820a6f40
K45-94 <kelvinhiuhu@live.com>
2022-04-20 16:40:40 +0300

    message

---
+++ src/pages/Profile/Payments/PageMpesa.vue
@@ -0,0 +1,126 @@
+<template>
+  <page>
+    <page-header>
+      <template #button-left>
+        <page-header-button-back-left to="/profile/payments" label="payments" />
+      </template>
+      <template #title> MPESA </template>
+    </page-header>
+    <page-body>
+      <div class="constrain q-pt-lg q-pb-md q-pl-lg q-pr-lg">
+        <div class="row q-col-gutter-lg">
+          <div class="col-12 col-sm-8">
+            <q-card>
+              <q-item>
+                <q-item-section avatar class="col-4">
+                  <q-avatar color="secondary" text-color="white"> SM </q-avatar>
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-item-label class="text-bold absolute-center"
+                    >Send Money</q-item-label
+                  >
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-btn
+                    to="/sendMoney"
+                    class="absolute-right"
+                    icon="eva-arrow-ios-forward-outline"
+                    flat
+                    dense
+                    color="black"
+                  />
+                </q-item-section>
+              </q-item>
+            </q-card>
+            <q-space class="q-pa-sm" />
+            <q-card>
+              <q-item>
+                <q-item-section avatar class="col-4">
+                  <q-avatar color="secondary" text-color="white"> WC </q-avatar>
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-item-label class="text-bold absolute-center"
+                    >Withdraw Cash</q-item-label
+                  >
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-btn
+                    to="/withdrawCash"
+                    class="absolute-right"
+                    icon="eva-arrow-ios-forward-outline"
+                    flat
+                    dense
+                    color="black"
+                  />
+                </q-item-section>
+              </q-item>
+            </q-card>
+            <q-space class="q-pa-sm" />
+            <q-card>
+              <q-item>
+                <q-item-section avatar class="col-4">
+                  <q-avatar color="secondary" text-color="white"> LM </q-avatar>
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-item-label class="text-bold absolute-center"
+                    >Lipa na M-PESA</q-item-label
+                  >
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-btn
+                    to="/TelkomWallet"
+                    class="absolute-right"
+                    icon="eva-arrow-ios-forward-outline"
+                    flat
+                    dense
+                    color="black"
+                  />
+                </q-item-section>
+              </q-item>
+            </q-card>
+            <q-space class="q-pa-sm" />
+            <q-card>
+              <q-item>
+                <q-item-section avatar class="col-4">
+                  <q-avatar color="secondary" text-color="white"> B </q-avatar>
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-item-label class="text-bold absolute-center"
+                    >Balance</q-item-label
+                  >
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-btn
+                    to="/PayPalWallet"
+                    class="absolute-right"
+                    icon="eva-arrow-ios-forward-outline"
+                    flat
+                    dense
+                    color="black"
+                  />
+                </q-item-section>
+              </q-item>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </page-body>
+  </page>
+</template>
+
+<script>
+import Page from "src/components/PagePlumComponent/Page.vue";
+import PageHeader from "src/components/PagePlumComponent/PageHeader.vue";
+import PageHeaderButtonBackLeft from "src/components/PagePlumComponent/PageHeaderButtonBackLeft.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: [Page, PageHeader, PageHeaderButtonBackLeft],
+  name: "mpesawallet",
+});
+</script>
+
+<style lang="sass">
+.q-card
+  background: linear-gradient(90deg,#CCC5B9 79%,#CCC5B9 15%)
+</style>
---
+++ src/pages/Profile/Payments/PagePayments.vue
@@ -0,0 +1,154 @@
+<template>
+  <page>
+    <page-header>
+      <template #button-left>
+        <page-header-button-back-left to="/profile" label="profile" />
+      </template>
+      <template #title> PAYMENTS </template>
+    </page-header>
+    <page-body>
+      <div class="q-pt-lg q-pb-md q-pl-lg q-pr-lg">
+        <div class="col-12 col-sm-8">
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> M </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >Mpesa</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/profile/payments/mpesawallet"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                >
+                  <q-badge color="green" floating>5</q-badge>
+                </q-btn>
+              </q-item-section>
+            </q-item>
+          </q-card>
+          <q-space class="q-pa-sm" />
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> A </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >Airtel Money</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/AirtelWallet"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                />
+              </q-item-section>
+            </q-item>
+          </q-card>
+          <q-space class="q-pa-sm" />
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> T </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >T-kash</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/TelkomWallet"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                />
+              </q-item-section>
+            </q-item>
+          </q-card>
+          <q-space class="q-pa-sm" />
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> PP </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >PayPal</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/PayPalWallet"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                >
+                  <q-badge color="blue" floating>3</q-badge>
+                </q-btn>
+              </q-item-section>
+            </q-item>
+          </q-card>
+
+          <q-space class="q-pa-sm" />
+          <q-card>
+            <q-item>
+              <q-item-section avatar class="col-4">
+                <q-avatar color="secondary" text-color="white"> P </q-avatar>
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-item-label class="text-bold absolute-center"
+                  >Pesalink</q-item-label
+                >
+              </q-item-section>
+              <q-item-section class="col-4">
+                <q-btn
+                  to="/Pesalink"
+                  class="absolute-right"
+                  icon="eva-arrow-ios-forward-outline"
+                  flat
+                  dense
+                  color="black"
+                >
+                  <q-badge color="orange" floating>2</q-badge>
+                </q-btn>
+              </q-item-section>
+            </q-item>
+          </q-card>
+        </div>
+      </div>
+    </page-body>
+  </page>
+</template>
+
+<script>
+import Page from "src/components/PagePlumComponent/Page.vue";
+import PageHeader from "src/components/PagePlumComponent/PageHeader.vue";
+import PageHeaderButtonBackLeft from "src/components/PagePlumComponent/PageHeaderButtonBackLeft.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: [Page, PageHeader, PageHeaderButtonBackLeft],
+  name: "PagePayments",
+});
+</script>
+
+<style lang="sass">
+.q-card
+  background: linear-gradient(90deg,#CCC5B9 79%,#CCC5B9 15%)
+</style>

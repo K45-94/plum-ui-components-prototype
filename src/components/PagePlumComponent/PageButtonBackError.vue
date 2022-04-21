8889eb87f68f8d6e6a3e1af9c3e8529afeee287b
K45-94 <kelvinhiuhu@live.com>
2022-04-21 07:02:54 +0300

    message

---
+++ src/components/PagePlumComponent/PageButtonBackError.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-btn to="/" icon="chevron_left" color="accent" flat rounded dense no-caps />
+</template>
+
+<script>
+export default {
+  name: "PageButtonBackError",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="sass"></style>

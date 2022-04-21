edf3d0dbd89a0225cf6cb3abea2a67d9f640449b
K45-94 <kelvinhiuhu@live.com>
2022-04-21 07:08:10 +0300

    message

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,29 +1,24 @@
 <template>
   <div
-    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
+    class="fullscreen bg-warning text-white text-center q-pa-md flex flex-center"
   >
     <div>
       <div style="font-size: 30vh">404</div>
 
       <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+      <div>
+        <page-button-back-error label="Go Back" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import PageButtonBackError from "src/components/PagePlumComponent/PageButtonBackError.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  components: [PageButtonBackError],
   name: "Error404",
 });
 </script>

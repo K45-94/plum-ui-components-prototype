90cbbf2f80eb762f24ebf29f8a06684cedb05c18
K45-94 <kelvinhiuhu@live.com>
2022-04-23 10:12:22 +0300

    message

---
+++ src/components/PagePlumComponent/PlumMpesaSendMoneyFormComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-form>
+    <q-input
+      v-model="formData.phoneNumber"
+      class="q-mb-md"
+      outlined
+      type="tel"
+      label="Phone Number"
+    />
+    <q-input
+      v-model="formData.amount"
+      class="q-mb-md"
+      outlined
+      type="number"
+      label="Amount"
+    />
+    <div class="row">
+      <q-space />
+      <q-btn color="primary" @click="submitForm" label="send money" />
+    </div>
+  </q-form>
+</template>
+
+<script>
+export default {
+  name: "PlumMpesaSendMoneyFormComponent",
+  props: ["tab"],
+  data() {
+    return {
+      formData: {
+        phoneNumber: "",
+        amount: "",
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      if (this.tab == "sendmoney") {
+        console.log("Send Money");
+      } else {
+        console.log("Other Network");
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass"></style>

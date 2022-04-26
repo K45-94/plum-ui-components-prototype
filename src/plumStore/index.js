d84a84ed2eaee30828562a982c34e99d12d96bc5
K45-94 <kelvinhiuhu@live.com>
2022-04-26 11:42:50 +0300

    message

--- src/plumStore/index.js
+++ src/plumStore/index.js
@@ -28,6 +28,64 @@ const state = reactive({
       icon: "eva-square",
     },
   ],
+
+  feeds: [
+    {
+      id: "id7",
+      url: "/images/7.jpg",
+      caption: "Thika Road at Night",
+    },
+    {
+      id: "id8",
+      url: "/images/8.jpg",
+      caption: "Ngong sunset with Pauline",
+    },
+    {
+      id: "id9",
+      url: "/images/9.jpg",
+      caption: "Tatu City",
+    },
+    {
+      id: "id14",
+      url: "/images/14.jpg",
+      caption: "Tatu City",
+    },
+    {
+      id: "id18",
+      url: "/images/18.jpg",
+      caption: "Tatu City",
+    },
+    {
+      id: "id24",
+      url: "/images/24.jpg",
+      caption: "Tatu City",
+    },
+    {
+      id: "id27",
+      url: "/images/27.jpg",
+      caption: "Tatu City",
+    },
+    {
+      id: "id28",
+      url: "/images/28.jpg",
+      caption: "Tatu City",
+    },
+    {
+      id: "id29",
+      url: "/images/29.jpg",
+      caption: "Tatu City",
+    },
+    {
+      id: "id31",
+      url: "/images/31.jpg",
+      caption: "Tatu City",
+    },
+    {
+      id: "id32",
+      url: "/images/32.jpg",
+      caption: "Tatu City",
+    },
+  ],
   usePageTransition: true,
 });
 

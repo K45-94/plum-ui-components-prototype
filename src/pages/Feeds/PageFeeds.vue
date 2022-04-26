a065487dc54ba30af16470d6fcc5d8ed8651550f
K45-94 <kelvinhiuhu@live.com>
2022-04-26 11:41:19 +0300

    message

---
+++ src/pages/Feeds/PageFeeds.vue
@@ -0,0 +1,34 @@
+<template>
+  <page>
+    <page-header>
+      <template #title> PLUM </template>
+    </page-header>
+    <page-body
+      ><div class="q-pt-lg q-pb-md q-pl-sm q-pr-sm row">
+        <router-link
+          v-for="feeds in store.state.feeds"
+          :key="feeds.id"
+          :to="`/feeds/${feeds.id}`"
+          class="col-10 q-ma-sm q-pr-sm"
+        >
+          <q-img :src="feeds.url" :ratio="1" />
+        </router-link></div
+    ></page-body>
+  </page>
+</template>
+
+<script>
+import Page from "src/components/PagePlumComponent/Page.vue";
+import store from "src/plumStore";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { Page },
+  name: "PageFeeds",
+  setup() {
+    return {
+      store,
+    };
+  },
+});
+</script>
---
+++ src/pages/Feeds/PagePost.vue
@@ -0,0 +1,36 @@
+<template>
+  <page>
+    <page-header>
+      <template #button-left>
+        <page-header-button-back-left label="feeds" />
+      </template>
+      <template #title> POST </template>
+    </page-header>
+    <page-body>
+      <div class="q-pt-lg q-pb-md q-pl-lg q-pr-lg">
+        <p v-for="i in 30" :key="i">
+          Building Plum with vue 3. This app is getting complex and I thought I
+          could redesign and rebuild it better with Vue 3 Finally found a way
+          for nesting child pages in routes.
+        </p>
+      </div>
+    </page-body>
+  </page>
+</template>
+
+<script>
+import Page from "src/components/PagePlumComponent/Page.vue";
+import PageHeaderButtonBackLeft from "src/components/PagePlumComponent/PageHeaderButtonBackLeft.vue";
+import store from "src/plumStore";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { Page, PageHeaderButtonBackLeft },
+  name: "PagePost",
+  setup() {
+    return {
+      store,
+    };
+  },
+});
+</script>

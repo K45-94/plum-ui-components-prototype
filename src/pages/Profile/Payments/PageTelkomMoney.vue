6c736994a46db89420ec28c58c0d66940fbdde25
K45-94 <kelvinhiuhu@live.com>
2022-05-13 13:45:35 +0300

    message

---
+++ src/pages/Profile/Payments/PageTelkomMoney.vue
@@ -0,0 +1,113 @@
+<template>
+  <page>
+    <page-header>
+      <template #button-left>
+        <page-header-button-back-left label="payments" />
+      </template>
+      <template #title> TELKOM MONEY </template>
+    </page-header>
+    <page-body>
+      <div class="constrain q-pt-lg q-pb-md q-pl-lg q-pr-lg">
+        <div class="row q-col-gutter-lg">
+          <div class="col-12 col-sm-8">
+            <q-list class="full-width">
+              <q-item
+                to="/profile/payments/telkomwallet/sendtelkommoney"
+                clickable
+              >
+                <q-item-section avatar class="col-4">
+                  <q-avatar color="secondary" text-color="white"> TK </q-avatar>
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-item-label class="text-bold absolute-center"
+                    >Send From T-kash</q-item-label
+                  >
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-btn
+                    to="/profile/payments/telkomwallet/sendtelkommoney"
+                    class="absolute-right"
+                    icon="eva-arrow-ios-forward-outline"
+                    flat
+                    dense
+                    color="black"
+                  />
+                </q-item-section>
+              </q-item>
+              <q-space class="q-pa-sm" />
+              <q-item to="/withdrawCash" clickable>
+                <q-item-section avatar class="col-4">
+                  <q-avatar color="secondary" text-color="white"> WC </q-avatar>
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-item-label class="text-bold absolute-center"
+                    >Withdraw Cash From Telkom</q-item-label
+                  >
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-btn
+                    to="/withdrawCash"
+                    class="absolute-right"
+                    icon="eva-arrow-ios-forward-outline"
+                    flat
+                    dense
+                    color="black"
+                  />
+                </q-item-section>
+              </q-item>
+              <q-space class="q-pa-sm" />
+              <q-item to="/TelkomWallet" clickable>
+                <q-item-section avatar class="col-4">
+                  <q-avatar color="secondary" text-color="white"> LT </q-avatar>
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-item-label class="text-bold absolute-center"
+                    >Pay With T-Kash</q-item-label
+                  >
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-btn
+                    to="/TelkomWallet"
+                    class="absolute-right"
+                    icon="eva-arrow-ios-forward-outline"
+                    flat
+                    dense
+                    color="black"
+                  />
+                </q-item-section>
+              </q-item>
+              <q-space class="q-pa-sm" />
+              <q-item to="/PayPalWallet" clickable>
+                <q-item-section avatar class="col-4">
+                  <q-avatar color="secondary" text-color="white"> B </q-avatar>
+                </q-item-section>
+                <q-item-section class="col-4">
+                  <q-item-label class="text-bold absolute-center"
+                    >T-Kash Balance</q-item-label
+                  >
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+        </div>
+      </div>
+    </page-body>
+  </page>
+</template>
+
+<script>
+import Page from "src/components/PagePlumComponent/Page.vue";
+import PageHeader from "src/components/PagePlumComponent/PageHeader.vue";
+import PageHeaderButtonBackLeft from "src/components/PagePlumComponent/PageHeaderButtonBackLeft.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: [Page, PageHeader, PageHeaderButtonBackLeft],
+  name: "telkomwallet",
+});
+</script>
+
+<style lang="sass">
+.q-card
+  background: linear-gradient(90deg,#CCC5B9 79%,#CCC5B9 15%)
+</style>

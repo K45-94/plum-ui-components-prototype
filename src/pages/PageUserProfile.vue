37ca325d9ee6c1547ab0f2537fce3ea96d9824d6
K45-94 <kelvinhiuhu@live.com>
2022-05-17 18:45:49 +0300

    message

---
+++ src/pages/PageUserProfile.vue
@@ -0,0 +1,24 @@
+<template>
+  <page>
+    <page-header>
+      <template #button-left>
+        <page-button-back label="" />
+      </template>
+
+      <template #title> USER-ID </template></page-header
+    >
+    <page-body></page-body>
+  </page>
+</template>
+
+<script>
+import Page from "src/components/PagePlumComponent/Page.vue";
+import PageHeader from "src/components/PagePlumComponent/PageHeader.vue";
+import PageButtonBack from "src/components/PagePlumComponent/PageButtonBack.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { Page, PageHeader, PageButtonBack },
+  name: "PageUserProfile",
+});
+</script>

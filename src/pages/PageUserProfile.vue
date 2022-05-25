f76836c7170f1214689791506d6a75d733f16c64
K45-94 <kelvinhiuhu@live.com>
2022-05-25 19:29:13 +0300

    message

--- src/pages/PageUserProfile.vue
+++ src/pages/PageUserProfile.vue
@@ -8,7 +8,49 @@
       <template #title> USER-ID </template></page-header
     >
     <page-body>
-      <p>user profile</p>
+      <div class="q-pt-sm q-pb-sm q-pl-none q-pr-none row">
+        <q-card class="bio col-11 q-ma-xs q-pb-md" flat>
+          <q-card-section
+            class="q-pt-xs q-pl-none q-pr-none q-pb-none"
+            vertical
+          >
+            <q-card-section class="q-pt-xs q-pl-none q-pr-none q-pb-none">
+              <q-item class="q-pt-xs q-pl-none q-pr-none q-pb-none" to="">
+                <q-item-section avatar>
+                  <q-btn round to="">
+                    <q-avatar>
+                      <img src="/images/7.jpg" />
+                    </q-avatar>
+                  </q-btn>
+                </q-item-section>
+
+                <q-item-section>
+                  <q-item-label class="text-bold">Plum Account</q-item-label>
+                  <q-item-label caption> post location </q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item to="" clickable class="q-pa-none">
+                <div class="text-black">
+                  A microblogging platform with very minimal user interactions.
+                  No like buttons, no reposts/retweets. Propaganda or fake news
+                  can be limited because posts are not supposed to go viral.
+                </div></q-item
+              >
+            </q-card-section>
+
+            ></q-card-section
+          >
+        </q-card>
+      </div>
+      <div class="row">
+        <q-img
+          v-for="feeds in store.state.feeds"
+          :key="feeds.id"
+          :src="feeds.url"
+          :ratio="1"
+          class="col-6"
+        />
+      </div>
     </page-body>
   </page>
 </template>
@@ -30,3 +72,11 @@ export default defineComponent({
   },
 });
 </script>
+<style lang="sass">
+.bio
+    position: relative
+    left: 49%
+    transform: translateX(-49.7%)
+    background-image: linear-gradient(90deg,#CCC5B9 79%,#CCC5B9 15%)
+    border-radius: 0
+</style>

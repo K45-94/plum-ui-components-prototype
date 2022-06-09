aecb277f27e60740dcdd6614dc07bfb381bb0d88
K45-94 <kelvinhiuhu@live.com>
2022-06-09 11:02:29 +0300

    message

--- src/pages/PageUserProfile.vue
+++ src/pages/PageUserProfile.vue
@@ -8,38 +8,7 @@
       <template #title> USER-ID </template></page-header
     >
     <page-body>
-      <div class="q-pt-sm q-pb-sm q-pl-none q-pr-none row">
-        <q-card class="bio col-11 q-ma-xs q-pb-md" flat>
-          <q-card-section
-            class="q-pt-xs q-pl-none q-pr-none q-pb-none"
-            vertical
-          >
-            <q-card-section class="q-pt-xs q-pl-none q-pr-none q-pb-none">
-              <q-item class="q-pt-xs q-pl-none q-pr-none q-pb-none" to="">
-                <q-item-section avatar>
-                  <q-btn round to="">
-                    <q-avatar>
-                      <img src="/images/7.jpg" />
-                    </q-avatar>
-                  </q-btn>
-                </q-item-section>
-
-                <q-item-section>
-                  <q-item-label class="text-bold">Plum Account</q-item-label>
-                  <q-item-label caption> post location </q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item to="" clickable class="q-pa-none">
-                <div class="text-black">
-                  A microblogging platform with very minimal user interactions.
-                  No like buttons, no reposts/retweets. Propaganda or fake news
-                  can be limited because posts are not supposed to go viral.
-                </div></q-item
-              >
-            </q-card-section>
-          </q-card-section>
-        </q-card>
-      </div>
+      <div class="page-body-spacer-user-posts"></div>
       <div class="row q-pl-sm q-pr-sm">
         <q-img
           v-for="feeds in store.state.feeds"
@@ -49,6 +18,40 @@
           class="col-6 shadow-20"
         />
       </div>
+
+      <q-page-sticky position="top" :offset="[0, 0]">
+        <div class="page-body-spacer-header-constrained"></div>
+        <div class="q-pt-none q-pb-sm q-pl-none q-pr-none row">
+          <q-card class="bio col-12 q-mt-none q-pb-md" flat>
+            <q-card-section class="q-pt-xs q-pl-xs q-pr-xs q-pb-xs" vertical>
+              <q-card-section class="q-pt-xs q-pl-none q-pr-none q-pb-none">
+                <q-item class="q-pt-xs q-pl-none q-pr-none q-pb-none" to="">
+                  <q-item-section avatar>
+                    <q-btn round to="">
+                      <q-avatar>
+                        <img src="/images/7.jpg" />
+                      </q-avatar>
+                    </q-btn>
+                  </q-item-section>
+
+                  <q-item-section>
+                    <q-item-label class="text-bold">Plum Account</q-item-label>
+                    <q-item-label caption> post location </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item to="" clickable class="q-pa-none">
+                  <div class="text-black">
+                    A microblogging platform with very minimal user
+                    interactions. No like buttons, no reposts/retweets.
+                    Propaganda or fake news can be limited because posts are not
+                    supposed to go viral.
+                  </div></q-item
+                >
+              </q-card-section>
+            </q-card-section>
+          </q-card>
+        </div>
+      </q-page-sticky>
     </page-body>
   </page>
 </template>
@@ -72,9 +75,7 @@ export default defineComponent({
 </script>
 <style lang="sass">
 .bio
-    position: relative
-    left: 49%
-    transform: translateX(-49.7%)
+
     background-image: linear-gradient(90deg,#CCC5B9 79%,#CCC5B9 15%)
     border-radius: 0
 </style>

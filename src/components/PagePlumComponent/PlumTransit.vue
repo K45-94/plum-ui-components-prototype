d267b4bdeab2c90b73a3fde438910c218fea03de
K45-94 <kelvinhiuhu@live.com>
2022-06-28 02:21:31 +0300

    transit-component

---
+++ src/components/PagePlumComponent/PlumTransit.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="map" class="window-height"></div>
+</template>
+
+<script>
+import tt from "@tomtom-international/web-sdk-maps";
+
+export default {
+  name: "PlumTransit",
+  props: ["city", "apikey"],
+  mounted() {
+    tt.map({
+      container: "map",
+      key: this.apikey,
+    });
+  },
+};
+</script>

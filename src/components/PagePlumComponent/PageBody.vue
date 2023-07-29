922b901351f8ada28f88b085ec95627ed12ad6e4
K45-94 <kelvinhiuhu@live.com>
2023-07-29 11:02:19 +0300

    message

--- src/components/PagePlumComponent/PageBody.vue
+++ src/components/PagePlumComponent/PageBody.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-scroll-area class="page-body absolute-top fit constrain text-black">
+  <q-scroll-area class="page-body absolute-top fit constrain text-secondary">
     <div class="page-body-spacer-header"></div>
     <slot />
   </q-scroll-area>
